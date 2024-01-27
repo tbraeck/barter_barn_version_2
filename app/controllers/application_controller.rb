c324ef733c973af20e52dd95922eaea4c2b1d027
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-01-27 08:09:29 -0600

    application record, routes update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+    before_action :authorize
+    include ActionController::Cookies
+
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    private
+    def authorize
+        @current_user = User.find(session[:user_id])
+        render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+    end
+
+    def record_not_found_response
+        model_name = controller_name.classify
+        render json: { errors: "#{model_name} not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
