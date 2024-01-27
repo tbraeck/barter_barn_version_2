ec2710dc6d4766f887fead0d8dbd7c1ed8f61f3d
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-01-27 08:09:40 -0600

    applciation record update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,31 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  Rails.application.routes.draw do
+    resources :users
+    resources :frees
+    resources :services
+    resources :goods
+    resources :forums 
+    resources :communities
+    resources :messages, only: [:index, :create]
+  
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+  
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  
+    get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
+   
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    # get "up" => "rails/health#show", as: :rails_health_check
+  
+    # Defines the root path route ("/")
+    # root "posts#index"
+  end
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
